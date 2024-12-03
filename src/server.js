415cf8bde0085dafc3227b6354aea8d529d7f590
Oustaa <otailaba98@gmail.com>
2024-12-03 21:47:52 +0100

    fixing some vulnabilities and removing some comments

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
-const os = require("os");
 const http = require("http");
 const app = require("./app");
-const cluster = require("cluster");
 const mongoConnect = require("./config/mongodb.connect");
 const { connection } = require("mongoose");
 const server = http.createServer(app);
@@ -10,18 +8,6 @@ const server = http.createServer(app);
 connection.once("open", () => {
   console.log("MongoDB connected successfully");
   server.listen(8000, () => console.log("server started"));
-
-  // if (cluster.isMaster) {
-  //   console.log("Master has been started..");
-  //   const NUM_WORKERS = os.cpus().length;
-
-  //   for (let i = 0; i < 4; i++) {
-  //     cluster.fork();
-  //   }
-  // } else {
-  //   console.log("Worker process started");
-  //   server.listen(8000, () => console.log("server started"));
-  // }
 });
 
 connection.on("error", (err) => {
@@ -29,3 +15,4 @@ connection.on("error", (err) => {
 });
 
 mongoConnect();
+
